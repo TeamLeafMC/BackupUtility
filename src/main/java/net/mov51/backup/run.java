329f96cc31a9aada205347fc4c797580eb5864dc
mov <mov51@mov51.net>
2021-07-14 02:40:16 -0400

    Moved run to logger

--- src/main/java/net/mov51/backup/run.java
+++ src/main/java/net/mov51/backup/run.java
@@ -1,19 +1,23 @@
 package net.mov51.backup;
 
 import net.mov51.helpers.config.backupConfig;
+import org.apache.logging.log4j.LogManager;
 
 import java.nio.file.Path;
 
 import static net.mov51.helpers.PterodactylApiHelper.sendCommand;
 import static net.mov51.helpers.archiveHelper.archive;
+import static net.mov51.helpers.logHelper.logInfo;
 import static net.mov51.rsync.execute.sync;
 
 public class run {
-    public static void StartBackup(Path path){
 
-        //todo change to info logger
-        System.out.println("---------------------");
-        System.out.println("Running backup " + path.getFileName());
+    private static final org.apache.logging.log4j.Logger Logger = LogManager.getLogger("runBackupLogger");
+
+    public static void StartBackup(Path path){
+        
+        logInfo(Logger,"---------------------");
+        logInfo(Logger,"Running backup " + path.getFileName());
 
         //getting backup config object
         backupConfig config = new backupConfig(path.toString());
@@ -28,9 +32,8 @@ public class run {
         //sending command
         //todo change to command array processor
         sendCommand(config.getFinishCommand(), config);
-        //todo change to info logger
-        System.out.println("Backup " + path.getFileName() + " complete!");
-        System.out.println("---------------------");
+        logInfo(Logger,"Backup " + path.getFileName() + " complete!");
+        logInfo(Logger,"---------------------");
 
     }
 }
