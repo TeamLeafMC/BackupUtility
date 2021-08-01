b2b094df371ba00fd428d278e22050be63bfc529
mov <mov51@mov51.net>
2021-07-31 20:36:19 -0400

    Check that Archive Source is not empty
    Resolves #8

--- src/main/java/net/mov51/helpers/archiveHelper.java
+++ src/main/java/net/mov51/helpers/archiveHelper.java
@@ -9,6 +9,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import static net.mov51.helpers.dateHelper.getFileSafeDate;
+import static net.mov51.helpers.fileHelper.isDirEmpty;
 import static net.mov51.helpers.logHelper.*;
 
 public class archiveHelper {
@@ -19,12 +20,25 @@ public class archiveHelper {
         //todo return bool
         logInfo(Logger,"Starting archival!");
 
+        try{
+            if(isDirEmpty(Paths.get(sourceFile))){
+                //todo use archive failsafe
+                logError(Logger,"Archive Source Directory is empty! Skipping this backup cycle!");
+                //return false
+            }
+        }catch (IOException e) {
+            //todo use archive failsafe
+            logFatalE(Logger,e,"Archive source could not be checked!");
+            //return false
+        }
+
         if(Paths.get(backupLocation).toFile().mkdirs()){
             logInfo(Logger,"Backup location " + backupLocation + " created!");
         }
 
         Path finalArchiveName = Paths.get(backupLocation + File.separator + archiveName + getFileSafeDate() + ".zip");
         try {
+
             FileOutputStream fos = new FileOutputStream(String.valueOf(finalArchiveName));
             ZipOutputStream zipOut = new ZipOutputStream(fos);
             File fileToZip = new File(sourceFile);
@@ -34,17 +48,20 @@ public class archiveHelper {
             fos.close();
             if(finalArchiveName.toFile().exists()){
                 logInfo(Logger,"Archive " + archiveName + " successfully created with full file name " + finalArchiveName);
+                //return true
             }else{
+                //todo use archive failsafe
                 logFatal(Logger,"Archive " + archiveName + " could not be created!");
+                //return false
             }
         }catch (IOException e) {
             //todo use archive failsafe
             logFatalE(Logger,e,"Archive file could not be created!");
+            //return false
         }
     }
 
     private static void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) {
-        //todo return bool
         try {
             if (fileToZip.isHidden()) {
                 return;
@@ -74,7 +91,6 @@ public class archiveHelper {
             }
             fis.close();
         } catch (IOException e) {
-            //todo use fail safe
             //todo use archive failsafe
             logFatalE(Logger,e,"File " + fileName + " could not be zipped!");
         }
--- src/main/java/net/mov51/helpers/fileHelper.java
+++ src/main/java/net/mov51/helpers/fileHelper.java
@@ -3,7 +3,10 @@ package net.mov51.helpers;
 import org.apache.logging.log4j.LogManager;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import static net.mov51.helpers.logHelper.*;
 
@@ -31,6 +34,16 @@ public class fileHelper {
     public static boolean createDirs(String filePath){
         File targetFile = new File(filePath);
         return createDirs(targetFile);
+    }
+
+    public static boolean isDirEmpty(Path path) throws IOException {
+        if (Files.isDirectory(path)) {
+            try (Stream<Path> entries = Files.list(path)) {
+                return !entries.findFirst().isPresent();
+            }
+        }
+
+        return false;
     }
 
 }
